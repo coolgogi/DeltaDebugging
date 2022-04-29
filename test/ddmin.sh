869f49c22f5f54e5e14146c6eac3fdf50d1b8f6b
coolgogi <coolgogi1024@gmail.com>
2022-04-29 21:54:31 +0900

    update shell script, python

--- test/ddmin.sh
+++ test/ddmin.sh
@@ -1,68 +1,28 @@
-echo "ddmin algorithm test"
+p1=1.00
+p2=1.00
+sigma=5.0
+home=/home/suhyun/DeltaDebugging/
+r_ddmin=$home/bin/main
+input=$home/test/input/test2.xml
+target=$home/libxml2/xmllint
+outDIR=$PWD/p${p1}/sigma${sigma}/
 
-rm output/ddmin_output.txt
-rm complement
-rm substring
-rm temp
-touch output/ddmin_output.txt
+if [ ! -d $PWD/p${p1} ]; then
+        mkdir $PWD/p${p1}
+        mkdir $outDIR
+fi
 
-ASAN_OPTIONS=detect_leaks=0:halt_on_error=1 ../bin/main ../jsmn/jsondump input/dd.json 0.80 1.0 30.0
-mv temp output/p0.80/sigma30.0/1
-ASAN_OPTIONS=detect_leaks=0:halt_on_error=1 ../bin/main ../jsmn/jsondump input/dd.json 0.80 1.0 30.0
-mv temp output/p0.80/sigma30.0/2
-ASAN_OPTIONS=detect_leaks=0:halt_on_error=1 ../bin/main ../jsmn/jsondump input/dd.json 0.80 1.0 30.0
-mv temp output/p0.80/sigma30.0/3
-ASAN_OPTIONS=detect_leaks=0:halt_on_error=1 ../bin/main ../jsmn/jsondump input/dd.json 0.80 1.0 30.0
-mv temp output/p0.80/sigma30.0/4
-ASAN_OPTIONS=detect_leaks=0:halt_on_error=1 ../bin/main ../jsmn/jsondump input/dd.json 0.80 1.0 30.0
-mv temp output/p0.80/sigma30.0/5
-ASAN_OPTIONS=detect_leaks=0:halt_on_error=1 ../bin/main ../jsmn/jsondump input/dd.json 0.80 1.0 30.0
-mv temp output/p0.80/sigma30.0/6
-ASAN_OPTIONS=detect_leaks=0:halt_on_error=1 ../bin/main ../jsmn/jsondump input/dd.json 0.80 1.0 30.0
-mv temp output/p0.80/sigma30.0/7
-ASAN_OPTIONS=detect_leaks=0:halt_on_error=1 ../bin/main ../jsmn/jsondump input/dd.json 0.80 1.0 30.0
-mv temp output/p0.80/sigma30.0/8
-ASAN_OPTIONS=detect_leaks=0:halt_on_error=1 ../bin/main ../jsmn/jsondump input/dd.json 0.80 1.0 30.0
-mv temp output/p0.80/sigma30.0/9
-ASAN_OPTIONS=detect_leaks=0:halt_on_error=1 ../bin/main ../jsmn/jsondump input/dd.json 0.80 1.0 30.0
-mv temp output/p0.80/sigma30.0/10
-ASAN_OPTIONS=detect_leaks=0:halt_on_error=1 ../bin/main ../jsmn/jsondump input/dd.json 0.80 1.0 30.0
-mv temp output/p0.80/sigma30.0/11
-ASAN_OPTIONS=detect_leaks=0:halt_on_error=1 ../bin/main ../jsmn/jsondump input/dd.json 0.80 1.0 30.0
-mv temp output/p0.80/sigma30.0/12
-ASAN_OPTIONS=detect_leaks=0:halt_on_error=1 ../bin/main ../jsmn/jsondump input/dd.json 0.80 1.0 30.0
-mv temp output/p0.80/sigma30.0/13
-ASAN_OPTIONS=detect_leaks=0:halt_on_error=1 ../bin/main ../jsmn/jsondump input/dd.json 0.80 1.0 30.0
-mv temp output/p0.80/sigma30.0/14
-ASAN_OPTIONS=detect_leaks=0:halt_on_error=1 ../bin/main ../jsmn/jsondump input/dd.json 0.80 1.0 30.0
-mv temp output/p0.80/sigma30.0/15
-ASAN_OPTIONS=detect_leaks=0:halt_on_error=1 ../bin/main ../jsmn/jsondump input/dd.json 0.80 1.0 30.0
-mv temp output/p0.80/sigma30.0/16
-ASAN_OPTIONS=detect_leaks=0:halt_on_error=1 ../bin/main ../jsmn/jsondump input/dd.json 0.80 1.0 30.0
-mv temp output/p0.80/sigma30.0/17
-ASAN_OPTIONS=detect_leaks=0:halt_on_error=1 ../bin/main ../jsmn/jsondump input/dd.json 0.80 1.0 30.0
-mv temp output/p0.80/sigma30.0/18
-ASAN_OPTIONS=detect_leaks=0:halt_on_error=1 ../bin/main ../jsmn/jsondump input/dd.json 0.80 1.0 30.0
-mv temp output/p0.80/sigma30.0/19
-ASAN_OPTIONS=detect_leaks=0:halt_on_error=1 ../bin/main ../jsmn/jsondump input/dd.json 0.80 1.0 30.0
-mv temp output/p0.80/sigma30.0/20
-ASAN_OPTIONS=detect_leaks=0:halt_on_error=1 ../bin/main ../jsmn/jsondump input/dd.json 0.80 1.0 30.0
-mv temp output/p0.80/sigma30.0/21
-ASAN_OPTIONS=detect_leaks=0:halt_on_error=1 ../bin/main ../jsmn/jsondump input/dd.json 0.80 1.0 30.0
-mv temp output/p0.80/sigma30.0/22
-ASAN_OPTIONS=detect_leaks=0:halt_on_error=1 ../bin/main ../jsmn/jsondump input/dd.json 0.80 1.0 30.0
-mv temp output/p0.80/sigma30.0/23
-ASAN_OPTIONS=detect_leaks=0:halt_on_error=1 ../bin/main ../jsmn/jsondump input/dd.json 0.80 1.0 30.0
-mv temp output/p0.80/sigma30.0/24
-ASAN_OPTIONS=detect_leaks=0:halt_on_error=1 ../bin/main ../jsmn/jsondump input/dd.json 0.80 1.0 30.0
-mv temp output/p0.80/sigma30.0/25
-ASAN_OPTIONS=detect_leaks=0:halt_on_error=1 ../bin/main ../jsmn/jsondump input/dd.json 0.80 1.0 30.0
-mv temp output/p0.80/sigma30.0/26
-ASAN_OPTIONS=detect_leaks=0:halt_on_error=1 ../bin/main ../jsmn/jsondump input/dd.json 0.80 1.0 30.0
-mv temp output/p0.80/sigma30.0/27
-ASAN_OPTIONS=detect_leaks=0:halt_on_error=1 ../bin/main ../jsmn/jsondump input/dd.json 0.80 1.0 30.0
-mv temp output/p0.80/sigma30.0/28
-ASAN_OPTIONS=detect_leaks=0:halt_on_error=1 ../bin/main ../jsmn/jsondump input/dd.json 0.80 1.0 30.0
-mv temp output/p0.80/sigma30.0/29
-ASAN_OPTIONS=detect_leaks=0:halt_on_error=1 ../bin/main ../jsmn/jsondump input/dd.json 0.80 1.0 30.0
-mv temp output/p0.80/sigma30.0/30
+if [ ! -d $outDIR ]; then
+        mkdir $outDIR
+fi
+
+
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
+export LD_LIBRARY_PATH
+for i in {1001..1001}; do
+#       ASAN_OPTIONS=detect_leaks=0 $r_ddmin $target $input $p1 $p2 $sigma
+#       mv temp $outDIR/$i
+        echo "hello world"
+done
+
+wc -c $outDIR/* > result_wc
