86ccb1a242f983abc4ecf723b561d29aec45cf22
coolgogi <coolgogi@naver.com>
2022-05-25 09:38:41 +0900

    update multi process with queue

--- src/process/pcs_range.c
+++ src/process/pcs_range.c
@@ -13,35 +13,60 @@
 #include <sys/wait.h>
 #include <sys/param.h>
 #include <stdatomic.h>
-#include <errno.h>
-
-#define WAITING -1
 
 atomic_int answer_index = 0 ;
-sem_t sem ;
-
-int * begin_arr ;
 int range_size ;
+struct stat st ;
+FILE * read_file_ptr ;
+
+int * begin_queue ;
+int queue_index = 0 ;
+
+pthread_mutex_t mutex ;
 
 struct input {
         int index ;
         char * execute_file_path ;
         char * ans ;
 } ;
-  
-FILE * read_file_ptr ;
 
+int
+queue_push (int num, int process_num) {
+	if (queue_index < process_num) {
+		pthread_mutex_lock(&mutex) ;
+		begin_queue[queue_index] = num ;
+		queue_index++ ;
+		pthread_mutex_unlock(&mutex) ;
+		return 0 ;
+	}
+	return -1 ;
+}
+
+int
+queue_pop () {
+	if (queue_index > 0) {
+		pthread_mutex_lock(&mutex) ;
+		int rt = begin_queue[0] ;
+		queue_index-- ;
+		for (int i = 0 ; i < queue_index ; i++) {
+			begin_queue[i] = begin_queue[i + 1] ;
+		}
+		pthread_mutex_unlock(&mutex) ;
+		return rt ;
+	}
+	return -1 ;
+}
 
 int
 find_answer_string (char * stderr_path, char * ans) {
 	char buffer[1025] ;
 	memset(buffer, 0, 1025) ;
 	FILE * stderr_file_ptr = fopen(stderr_path, "r") ;	
-	struct stat st ;
-	stat(stderr_path, &st) ;
+	struct stat stderr_st ;
+	stat(stderr_path, &stderr_st) ;
 	
-	int div = st.st_size / 1024 ;
-	int mod = st.st_size % 1024 ;
+	int div = stderr_st.st_size / 1024 ;
+	int mod = stderr_st.st_size % 1024 ;
 	for (int i = 0 ; i < div ; i++) {
 		if (fread(buffer, 1024, 1, stderr_file_ptr) == 1) {
 			if (strstr(buffer, ans) != NULL) {
@@ -79,27 +104,20 @@ thread (void * arg) {
 	sprintf(complement_path, "complement%d", ip->index) ;
 	sprintf(stderr_path, "stderr%d", ip->index) ;
 
-//        FILE * read_file_ptr = fopen("temp", "r") ;
-	struct stat st ;
-	stat("temp", &st) ; 
-	int current_rs = range_size ;
         while (1) {
-		
 		int start ;
-		if (range_size == 0) {
-			break ;	
-		}
-
-		if (begin_arr[ip->index] == WAITING) {
-			sem_wait(&sem) ;
-		}
 		while (1) {
-			if (begin_arr[ip->index] != WAITING) {
-				start = begin_arr[ip->index] ;
-				begin_arr[ip->index] = WAITING ;
-				sem_post(&sem) ;
+			start = queue_pop() ;
+			fprintf(stderr, "index: %d, start: %d, queue_index: %d\n", ip->index, start, queue_index) ;
+			if (start != -1) {
 				break ;
 			}
+			if (range_size == 0) {
+				break ;
+			}
+		}
+		if (range_size == 0) {
+			break ;
 		}
 
 		int end = start + range_size ;
@@ -116,67 +134,48 @@ thread (void * arg) {
                         char temp_file_path[10] ;
                         sprintf(temp_file_path, "temp%d", index) ;
 		      	copy_file(complement_path, temp_file_path) ;
-		}
+		} 
         }
-        fclose(read_file_ptr) ;
-        void * p = NULL ;
-        return p ;	
+        return NULL ;	
 }
 
 void
 pcs_range (char * execute_file_path, char * answer, int process_num) {
+	pthread_mutex_init(&mutex, NULL) ;
+	begin_queue = (int *) malloc(sizeof(int) * process_num) ;
 
-	begin_arr = (int *) malloc(sizeof(int) * process_num) ;
-	sem_init(&sem, 0, process_num) ;
-	read_file_ptr = fopen("temp", "r") ;
-
-        struct input * ip[process_num] ;
+        read_file_ptr = fopen("temp", "r") ;
         pthread_t t[process_num] ;
+        struct input * ip[process_num] ;
         for (int i = 0 ; i < process_num ; i++) {
                 ip[i] = (struct input *) malloc(sizeof(struct input)) ;
                 ip[i]->index = i ;
 		ip[i]->execute_file_path = execute_file_path ;
 		ip[i]->ans = answer ;
-
-		begin_arr[i] = -1 ;	
-		pthread_create(&t[i], NULL, thread, (void *) ip[i]) ;
         }
 	
-	struct stat st ;
         stat("temp", &st) ;
 	int start_size = st.st_size - 1 ;
 	int current_size = 0 ;
 
+        for (int i = 0 ; i < process_num ; i++) {
+		pthread_create(&t[i], NULL, thread, (void *) ip[i]) ;
+	}
 	while (1) {
 		for (range_size = start_size ; range_size > 0 ; range_size--) {
+			fprintf(stderr, "range_size: %d\n", range_size) ;
 			for (int begin = 0 ; begin <= st.st_size - range_size ; begin++) {
-				int arr_i = 0 ;
+				int rt ;
 				while (1) {
-					if (begin_arr[arr_i] == WAITING) {
-						begin_arr[arr_i] = begin ;
-						arr_i = (arr_i + 1) % process_num ;
+					if (queue_push(begin, process_num) != -1) {
 						break ;
 					}
-					fprintf(stderr, "arr_i: %d, begin: %d\n", arr_i, begin) ;
-					arr_i = (arr_i + 1) % process_num ;
-				}
+				}	
 			}
-			//waiting thread
-			while (1) {
-				int flag = 1 ;
-				for (int i = 0 ; i < process_num ; i ++) {
-					if (begin_arr[i] != -1) {
-						flag = 0 ;
-					}
-				}
-				if (flag == 1) {
-					break ;
-				}
-			}
-			//
+
 	                if (answer_index > 0) {
 				current_size = range_size ;
-				break ;
+				break ; 
 	                }
 	        }
 		if (answer_index == 0) {
@@ -187,20 +186,21 @@ pcs_range (char * execute_file_path, char * answer, int process_num) {
 		 	char selected_path[10] ;
 	 		sprintf(selected_path, "temp%d", rnum) ;
 			copy_file(selected_path, "temp") ;
+			fclose(read_file_ptr) ;
+			read_file_ptr = fopen("temp", "r") ;
+
 			stat("temp", &st) ;
 			start_size = MIN(current_size, st.st_size - 1) ;
 			answer_index = 0 ;
-			fclose(read_file_ptr) ;
-			read_file_ptr = fopen("temp", "r") ;
 		}
 	}
 	range_size = 0 ;
-
-	for (int i = 0 ; i < process_num ; i++) {
+        for (int i = 0 ; i < process_num ; i++) {
 		pthread_join(t[i], NULL) ;
                 free(ip[i]) ;
-	}
-
-	free(begin_arr) ;
+        }
 	fclose(read_file_ptr) ;
+	free(begin_queue) ;
+	pthread_mutex_destroy(&mutex) ;
 }
+
